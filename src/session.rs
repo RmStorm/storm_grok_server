6303f0c6f2e01c70b522b00931f3747788a73af2
Roald Storm <roald.storm@gmail.com>
2022-10-09 14:39:21 +0200

    Added support for tcp connections

--- src/session.rs
+++ src/session.rs
@@ -13,7 +13,7 @@ use uuid::Uuid;
 
 use crate::{google_key_store, server, settings};
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Mode {
     Http,
     Tcp,
@@ -120,10 +120,10 @@ impl Handler<StartListeningOnPort> for StormGrokClientSession {
     }
 }
 
-async fn listen_available_port() -> Result<TcpListener> {
+async fn listen_available_port(local_addr: &str) -> Result<TcpListener> {
     debug!("Finding available port");
     for port in 1025..65535 {
-        match TcpListener::bind(("127.0.0.1", port)).await {
+        match TcpListener::bind((local_addr, port)).await {
             Ok(l) => return Ok(l),
             Err(error) => match error.kind() {
                 ErrorKind::AddrInUse => {}
@@ -132,6 +132,20 @@ async fn listen_available_port() -> Result<TcpListener> {
         }
     }
     bail!("No ports available")
+}
+
+async fn start_local_tcp_server(mode: Mode) -> Result<TcpListener> {
+    let local_addr = match mode {
+        Mode::Tcp => "0.0.0.0",
+        Mode::Http => "127.0.0.1",
+    };
+    match listen_available_port(local_addr).await {
+        Ok(l) => Ok(l),
+        Err(e) => {
+            error!("Error while finding free port for new client: {:?}", e);
+            bail!("internal server error, could not find free port for you");
+        }
+    }
 }
 
 pub async fn start_session(
@@ -165,31 +179,24 @@ async fn do_handshake(
     key_store_address: Addr<google_key_store::GoogleKeyStore>,
     auth: settings::AuthRules,
 ) -> Result<()> {
-    let id = Uuid::new_v4();
+    let conn = new_conn.bi_streams.next().await.ok_or(anyhow!("No conn"))?;
+    let (mut send, recv) = conn?;
+    let received_bytes = recv.read_to_end(1000).await?;
+    let requested_mode = Mode::from(received_bytes[0]);
 
-    let tcp_listener = match listen_available_port().await {
-        Ok(l) => l,
-        Err(e) => {
-            error!("Error while finding free port for new client: {:?}", e);
-            bail!("internal server error, could not find free port for you");
-        }
-    };
+    let tcp_listener = start_local_tcp_server(requested_mode).await?;
     let tcp_addr = tcp_listener.local_addr()?;
     debug!("Reserved: {:?} for new client", &tcp_addr);
 
-    if let Some(Ok((mut send, recv))) = new_conn.bi_streams.next().await {
-        let received_bytes = recv.read_to_end(1000).await?;
-        info!("First byte = {:?}", Mode::from(received_bytes[0]));
-        let token = String::from_utf8_lossy(&received_bytes[1..]);
-        let kid = decode_header(&token)?
-            .kid
-            .ok_or(anyhow!("No kid found in token header"))?;
-        let dec_key = google_key_store::get_key_for_kid(key_store_address, kid).await?;
-        let token_message = decode::<Claims>(&token, &dec_key, &Validation::new(Algorithm::RS256))?;
-        validate_claims(token_message.claims, auth).await?;
-        send.write_all(id.as_bytes()).await?;
-        send.finish().await?;
-    }
+    let token = String::from_utf8_lossy(&received_bytes[1..]);
+    let kid = decode_header(&token)?
+        .kid
+        .ok_or(anyhow!("No kid found in token header"))?;
+    let dec_key = google_key_store::get_key_for_kid(key_store_address, kid).await?;
+    let token_message = decode::<Claims>(&token, &dec_key, &Validation::new(Algorithm::RS256))?;
+    validate_claims(token_message.claims, auth).await?;
+
+    let id = Uuid::new_v4();
 
     let session_address = StormGrokClientSession {
         id: id,
@@ -209,6 +216,13 @@ async fn do_handshake(
             session_data: (session_address.clone(), tcp_addr.to_string()),
         })
         .await?;
+
+    match requested_mode {
+        Mode::Tcp => send.write_all(&tcp_addr.port().to_be_bytes()).await?,
+        Mode::Http => send.write_all(id.as_bytes()).await?,
+    }
+    send.finish().await?;
+
     Ok(())
 }
 
